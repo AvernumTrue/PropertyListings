84aebd4e4d418279f077f5e076c831f4411c54a8
Andre Serfontein <andre.serfontein5@gmail.com>
2022-02-11 15:32:53 +0200

    add blank components for main menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HomesForSaleComponent } from './homes-for-sale/homes-for-sale.component';
+import { HomeComponent } from './home/home.component';
+import { SaleListComponent } from './sale-list/sale-list.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { SubMenuComponent } from './sub-menu/sub-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HomesForSaleComponent,
+    HomeComponent,
+    SaleListComponent,
+    RegisterComponent,
+    LoginComponent,
+    MainMenuComponent,
+    SubMenuComponent
   ],
   imports: [
     BrowserModule,
