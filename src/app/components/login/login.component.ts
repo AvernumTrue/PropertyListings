dd9278f3e32c6b34dd29a7d028716994a10f0362
Andre Serfontein <andre.serfontein5@gmail.com>
2022-02-16 13:31:32 +0200

    rebomove infinite while loop

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -50,31 +50,31 @@ export class LoginComponent implements OnInit {
         console.log('All Users', users);
 
         let correctUser = 0;
-        while (correctUser === 0) {
-          for (const user of users) {
+        // while (correctUser === 0) {
+        for (const user of users) {
 
-            const enteredEmail = this.loginForm.get('email').value;
-            const enteredPassword = this.loginForm.get('password').value;
+          const enteredEmail = this.loginForm.get('email').value;
+          const enteredPassword = this.loginForm.get('password').value;
 
-            if (user.email === enteredEmail) {
-              console.log(`Email Matches`);
-              if (user.password === enteredPassword) {
-                console.log(`User Matches`);
-                correctUser = 1;
-                // authenticate(user);
-                // TODO set user as authenticated
-              } else {
-                const correctPassword = false
-                // console.log(`Incorrect Password`)
-              }
+          if (user.email === enteredEmail) {
+            console.log(`Email Matches`);
+            if (user.password === enteredPassword) {
+              console.log(`User Matches`);
+              correctUser = 1;
+              // authenticate(user);
+              // TODO set user as authenticated
             } else {
-              const correctEmail = false
-              // console.log(`Email not found`)
+              const correctPassword = false
+              // console.log(`Incorrect Password`)
             }
-            console.log(user.email);
-            console.log(user.password);
+          } else {
+            const correctEmail = false
+            // console.log(`Email not found`)
           }
+          console.log(user.email);
+          console.log(user.password);
         }
+        // }
       },
       error: err => {
         console.log(err);
