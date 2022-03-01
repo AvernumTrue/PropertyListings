5a38891ebcf62029e476e0832ab657ab1ff6e253
Andre Serfontein <andre.serfontein5@gmail.com>
2022-03-01 10:18:15 +0200

    initial sale list page

--- src/app/components/sale-list/sale-list.component.ts
+++ src/app/components/sale-list/sale-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Spinkit } from 'ng-http-loader';
+import { Advert } from 'src/app/models/advert.model';
+import { AdvertService } from 'src/app/services/advert.service';
 
 @Component({
   selector: 'pl-sale-list',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SaleListComponent implements OnInit {
 
-  constructor() { }
+  spinnerStyle = Spinkit;
+  adverts: Advert[] = [];
+  loading: boolean;
+
+  constructor(
+    private advertService: AdvertService,
+    private router: Router,) {
+  };
 
   ngOnInit(): void {
+    this.getAdverts()
+  }
+
+  getAdverts() {
+    this.loading = true;
+    this.advertService.getAdverts().subscribe({
+      next: adverts => {
+        this.adverts = adverts;
+        this.loading = false;
+      }
+    });
   }
 
 }
